b909d54e256ac85a14f5104c6533d5c517f7de25
mariibx <rm99667@estudante.fieb.edu.br>
2026-02-21 01:18:48 -0300

    commit limpo sem token

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("clientReady", () => {
+  console.log(`✅ Bot online como ${client.user.tag}`);
+});
+
+client.on("messageCreate", message => {
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
